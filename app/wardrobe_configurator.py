f9197552369212fa16406cdea3c69ccdeb1cc63d
Karsten Lettow <kasitmp@gmail.com>
2025-03-14 17:36:40 +0100

    Add 50cm wardrobe element

--- app/wardrobe_configurator.py
+++ app/wardrobe_configurator.py
@@ -1,4 +1,8 @@
 class Wall:
-  def __init__(self):
-    self.width = 250
+    def __init__(self):
+        self.width = 250
 
+
+class WardrobeElement:
+    def __init__(self):
+        self.width = 50
--- tests/test_wardrobe_configurator.py
+++ tests/test_wardrobe_configurator.py
@@ -1,5 +1,11 @@
-from app.wardrobe_configurator import Wall
+from app.wardrobe_configurator import Wall, WardrobeElement
+
 
 def test_width_of_Wall():
-  wall = Wall()
-  assert wall.width == 250
+    wall = Wall()
+    assert wall.width == 250
+
+
+def test_width_of_WardrobeElement():
+    wardrobe_element = WardrobeElement()
+    assert wardrobe_element.width == 50
